04f8954fd400aeab9cacc55030a24ac6e56b667c
Anurag Balakrishnan <anuragbala8@gmail.com>
2022-05-17 16:41:34 +0530

    Adding

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Prints from "./Pages/Prints/Prints"
+import Contact from './Pages/Contact/Contact';
+import Home from './Pages/Home/Home';
+import Header from './Pages/Header/Header';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/prints" element={<Prints />} />
+          <Route path="/contact" element={<Contact />} />            
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/Pages/Header/Header.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import "./Header.css"
+import SignImg from "../../Assets/K_13_copy1_300x.webp"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBagShopping, faSearch, faUser } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+    return (
+        <div>
+            <div className='container-fluid'>
+                <div className='row'>
+                    <div className='col-4'>
+
+                    </div>
+                    <div className='col-4 text-center'>
+                        <Link to="/"><img src={SignImg} className="img-fluid" alt="Signature" /></Link>
+                    </div>
+                    <div className='col-4 my-5 d-flex justify-content-end'>
+                        <FontAwesomeIcon icon={faSearch} className="mt-4 mx-3 icon-size" />
+                        <FontAwesomeIcon icon={faBagShopping} className="mt-4 mx-3 icon-size" />
+                        <FontAwesomeIcon icon={faUser} className="mt-4 mx-3 icon-size" />
+
+                    </div>
+                </div>
+            </div>
+            <div>
+                <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+                    <div className="container-fluid d-flex justify-content-center">
+                        <Link to="/" className='mx-4 dec-none text-light header-hover-effect py-2 fs-5'>
+                            Home
+                        </Link>
+                        <Link to="/prints" className='mx-4 dec-none text-light header-hover-effect py-2 fs-5'>
+                            Prints
+                        </Link><Link to="/contact" className='mx-4 dec-none text-light header-hover-effect py-2 fs-5'>
+                            Contact
+                        </Link>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    )
+}
+
+export default Header
